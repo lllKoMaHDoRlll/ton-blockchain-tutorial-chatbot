32d7fd5dd1145b4b47b241300c11e1bd30f5755f
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-09-01 13:20:36 +0300

    refactor commands handlers

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,34 @@
-import { Telegraf } from "telegraf";
+import { Context, Telegraf } from "telegraf";
 import dotenv from "dotenv";
-import { Address, beginCell, toNano } from "ton-core";
+import { Address, beginCell, Cell, toNano } from "ton-core";
 import qs from "qs";
 
 dotenv.config();
+
+const formTransactionLink = (text: string, amount: bigint, messageBody: Cell): string => {
+    return (
+        'https://app.tonkeeper.com/transfer/' +
+        Address.parse("EQBwYji2MLP888Rf8CsnM5OdvKPZSQvhpwSYz-u3tu4O-G6C").toString({testOnly: true}) + "?" +
+        qs.stringify({
+            text: text,
+            amount: amount.toString(10),
+            bin: messageBody.toBoc({idx: false}).toString("base64"),
+        })
+    );
+}
+
+const requestSigningTransaction = (ctx: Context, text: string, link: string) => {
+    ctx.reply(text, {
+        reply_markup: {
+            inline_keyboard: [
+                [{
+                    text: "Sign transaction",
+                    url: link
+                }],
+            ],
+        },
+    });
+}
 
 const bot = new Telegraf(process.env.TG_BOT_TOKEN!);
 
@@ -26,54 +51,30 @@ bot.hears("Increment by 3", (ctx) => {
         .storeUint(3, 32)
     .endCell();
     
-    let link = 'https://app.tonkeeper.com/transfer/' +
-        Address.parse("EQBwYji2MLP888Rf8CsnM5OdvKPZSQvhpwSYz-u3tu4O-G6C").toString({testOnly: true}) + "?" +
-        qs.stringify({
-            text: "Increment transaction",
-            amount: toNano("0.05").toString(10),
-            bin: messageBody.toBoc({idx: false}).toString("base64"),
-        });
+    let link = formTransactionLink("Increment transaction", toNano("0.05"), messageBody);
 
-    ctx.reply("To increment counter by 3, please sign a transaction:", {
-        reply_markup: {
-            inline_keyboard: [
-                [{
-                    text: "Sign transaction",
-                    url: link
-                }],
-            ],
-        },
-    });
+    requestSigningTransaction(ctx, "To increment counter by 3, please sign a transaction:", link);
 });
 
 bot.hears("Deposit 1 TON", (ctx) => {
     const messageBody = beginCell()
         .storeUint(2, 32)
     .endCell();
-    
-    let link = 'https://app.tonkeeper.com/transfer/' +
-        Address.parse("EQBwYji2MLP888Rf8CsnM5OdvKPZSQvhpwSYz-u3tu4O-G6C").toString({testOnly: true}) + "?" +
-        qs.stringify({
-            text: "Deposit funds",
-            amount: toNano("1").toString(10),
-            bin: messageBody.toBoc({idx: false}).toString("base64"),
-        });
 
-    ctx.reply("To deposit 1 TON, please sign a transaction:", {
-        reply_markup: {
-            inline_keyboard: [
-                [{
-                    text: "Sign transaction",
-                    url: link
-                }],
-            ],
-        },
-    });
+    let link = formTransactionLink("Deposit funds", toNano("1"), messageBody);
+
+    requestSigningTransaction(ctx, "To deposit 1 TON, please sign a transaction:", link);
 });
 
 bot.hears("Withdraw 1 TON", (ctx) => {
-    // sending withdraw transaction
-    ctx.reply("Withdrawn 1 TON");
+    const messageBody = beginCell()
+        .storeUint(3, 32)
+        .storeCoins(toNano("1"))
+    .endCell();
+    
+    let link = formTransactionLink("Withdraw funds", toNano("0.05"), messageBody);
+
+    requestSigningTransaction(ctx, "To withdraw 1 TON, please sign a transaction:", link);
 });
 
 bot.launch();
