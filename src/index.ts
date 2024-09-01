d59cf502b6a076577a769aa34e40e4933973a1e3
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-09-01 12:28:11 +0300

    implement /start command handling

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-
 const bot = new Telegraf(process.env.TG_BOT_TOKEN!);
+
+bot.start((ctx) => {
+    ctx.reply("Welcome to counter app!", {
+        reply_markup: {
+            keyboard: [
+                ["Increment by 3"],
+                ["Deposit 1 TON"],
+                ["Withdraw 1 TON"]
+            ],
+        },
+    });
+});
+
+bot.launch();
